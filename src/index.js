24b4919d042d9feaffecbd8a080b65d1baccd730
github username <github email address>
2020-03-24 10:12:49 -0500

    added chart detail and modal

--- src/index.js
+++ src/index.js
@@ -2,12 +2,48 @@ google.charts.load('current', {packages: ['geochart'], 'mapsApiKey': ['MAPSAPIKE
 
 const drawRegionsMap = () => {
     let data = google.visualization.arrayToDataTable([
-        ['Country', "Popularity"]
+        ['Country', "Number of Cases"],
+        ['US', 45000],
+        ['Germany', 23000],
+        ['China', 81000]
     ]);
-    let options = {};
+    let options = {
+        colorAxis: {minValue: 0,  colors: ['#34e8eb', '#3d34eb']}
+    };
     let chart = new google.visualization.GeoChart(document.getElementById('map'));
     
     chart.draw(data, options);
+
+
+    google.visualization.events.addListener(chart, 'ready', () => [
+        google.visualization.events.addListener(chart, 'regionClick', (e) => {
+            
+            const modal = document.getElementById("modal-1")
+            modal.checked = true;
+     
+            const renderModal = () => {
+                const h4 = document.querySelector("#region-modal-title");
+                h4.innerHTML = e.region;
+                //insert chart renderings and data here 
+
+            }
+
+            renderModal();
+
+            //this will rerender map zoomed into the region clicked and show provinces/states
+            // options = {
+            //     displayMode:'regions',
+            //     region: e.region,
+            //     resolution: "provinces"
+            // }
+            // chart.draw(data, options);
+        })
+
+
+    ]);
 }
 
 google.charts.setOnLoadCallback(drawRegionsMap);
+
+
+    
