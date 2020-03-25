83e256c2b1cb870cd5f5bd5866490256fa9ef66c
github username <github email address>
2020-03-24 19:10:18 -0500

    changed fetches to handle new data structure from rails

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-google.charts.load('current', {packages: ['geochart'], 'mapsApiKey': ["MAPSAPIKEY"]}); //load different packages for timeseries?
-const countriesUrl = "http://localhost:3000/all_data"; //will change to /countries/
+google.charts.load('current', {packages: ['geochart', 'corechart'], 'mapsApiKey': ['MAPSAPIKEY']}); //load different packages for timeseries?
+const countriesUrl = "http://localhost:3000/countries"; //need to embed countries with current days
+
+//===============  GET COUNTRY FUNCTIONS ========================//
 
 const getCountryData = () => {
     fetch(countriesUrl)
@@ -8,21 +10,15 @@ const getCountryData = () => {
         const map_array = [['Country', 'Number of Cases']];
         const modal_array = [];
         
+        //get country data
         countries.forEach(country => {
-            let exists = false;
-            map_array.forEach(row => {
-                if (country["Country/Region"] === row[0]){
-                    exists = true;
-                }
-            });
-            if (!exists){
-                let map_row = [country["Country/Region"], parseInt(country["Confirmed"])];
-                let modal_row = [country["Country/Region"], parseInt(country["Confirmed"]), parseInt(country["Deaths"]), parseInt(country["Recovered"])]
+            let lastDay = country.days.length - 1;
+            let map_row = [country.name, country.days[lastDay].cases];
+            let modal_row = [country.name, country.days[lastDay].cases, country.days[lastDay].deaths, country.days[lastDay].recovered, country.id];
 
-                if (map_row[1] > 0){
-                    map_array.push(map_row);
-                    modal_array.push(modal_row);
-                }
+            if (map_row[1] > 0){
+                map_array.push(map_row);
+                modal_array.push(modal_row);
             }
         });
 
@@ -33,13 +29,27 @@ const getCountryData = () => {
         .catch(err => console.log(err));
 }
 
+const getCountryByDaysData = (id) => {
+    return fetch(`${countriesUrl}/${id}`)
+        .then(res => res.json())
+        .then(days => days)
+        .catch(err => console.log(err));
+}
+
+
+//===============  DRAW MAP FUNCTION ========================//
+
+
 const drawRegionsMap = (display_array, data_array) => {
     let data = google.visualization.arrayToDataTable(display_array);
     let options = {
-        colorAxis: {minValue: 0,  colors: ['#34e8eb', '#3d34eb']}
+        colorAxis: {minValue: 0,  colors: ['#34e8eb', '#3d34eb']},
+        legend: {textStyle: {fontName: "Patrick Hand SC", fontSize: 11}},
+        tooltip: {textStyle: {fontName: "Patrick Hand SC"}}
     };
     let chart = new google.visualization.GeoChart(document.getElementById('map'));
 
+    //what happens on click for each region
     const selectHandler = () => {
        let selectedCountry = chart.getSelection()[0];
 
@@ -47,6 +57,9 @@ const drawRegionsMap = (display_array, data_array) => {
             let value = data.getValue(selectedCountry.row, 0);
             const modal = document.getElementById("modal-1")
             modal.checked = true;
+
+            //===============  RENDER MODEL & LINE CHART FUNCTION ========================//
+
 
             const renderModal = () => {
                 const h4 = document.querySelector("#region-modal-title");
@@ -60,10 +73,34 @@ const drawRegionsMap = (display_array, data_array) => {
                 recovered.innerHTML = current_country[3];
                 
                 //insert chart renderings and data here 
-                
+                const countryId = current_country[4];
+
+                //get days from country end point
+                const days = Promise.resolve(getCountryByDaysData(countryId));
+
+                days.then(days => {
+                    const lineChartArray = [['Day', 'Cases', 'Deaths', 'Recovered']];
+
+                    days.forEach(day => {
+                        const lineRow = [day.date, day.cases, day.deaths, day.recovered];
+                        lineChartArray.push(lineRow);
+                    })
+
+                    let lineData = google.visualization.arrayToDataTable(lineChartArray);
+                    let lineOptions = {
+                        title: "Corona Virus Summary",
+                        curveType: 'function',
+                        legend: {position: 'bottom'}
+                    }
+                    let lineChart = new google.visualization.LineChart(document.getElementById("curve-chart"));
+    
+                    lineChart.draw(lineData, lineOptions);
+                });
             }
             
             renderModal();
+
+            //===============  POTENTIAL ADD ON FUNCTIONALITY  ========================//
 
             //this will rerender map zoomed into the region clicked and show provinces/states
             // options = {
@@ -75,13 +112,16 @@ const drawRegionsMap = (display_array, data_array) => {
        }
     }
 
+    //===============  ADD SELECT HANDLER AND DRAW CHART ========================//
+
     google.visualization.events.addListener(chart, 'select', selectHandler);
     
     chart.draw(data, options);
 }
 
-getCountryData();
-// google.charts.setOnLoadCallback(drawRegionsMap);
+//===============  CALLBACK TO START FETCHING COUNTRY DATA ONCE LOADED ========================//
+
+google.charts.setOnLoadCallback(getCountryData);
 
 
     
